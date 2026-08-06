b7d15bd0f8b2544dd6fae8e05ef19f8244a41f40
Laksha Bansal <laksha.bansal7015@gmail.com>
2026-08-06 11:54:07 +0530

    Adding programs of access Modifier

---
+++ AccessModifier/AccessSpecifier1/Program1.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace AccessSpecifier1
+{
+    public class class1
+    {
+        internal void show()
+        {
+            Console.WriteLine("This is a internal method2..."); 
+        } 
+    }
+
+
+    class Program1 : class1
+    {
+        static void Main(string[] args)
+        {
+            Program1 c = new Program1();
+            c.show();
+            Console.ReadLine();
+        }
+    }
+}
---
+++ AccessModifier/AccessSpecifier2/Program2.cs
@@ -0,0 +1,18 @@
+﻿using AccessSpecifier1;
+using System;       
+
+namespace AccessSpecifier2
+{
+    class Program2 
+    {
+        public static void Main()
+        {
+            //Program2 p = new Program2();
+            //p.show();
+
+            class1 c = new class1();
+            c.show(); 
+            Console.ReadLine();
+        }
+    }
+}
